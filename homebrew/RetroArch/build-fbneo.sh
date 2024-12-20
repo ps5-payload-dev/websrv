6052baadf7d35127ffe63087dcb96bcbe69b027a
John Törnblom <john.tornblom@gmail.com>
2024-12-20 19:21:42 +0100

    retroarch: add build script for the fbneo core

---
+++ homebrew/RetroArch/build-fbneo.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#   Copyright (C) 2024 John Törnblom
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not see
+# <http://www.gnu.org/licenses/>.
+
+VER="master"
+URL="https://github.com/libretro/FBNeo/archive/refs/heads/master.tar.gz"
+INFO="https://raw.githubusercontent.com/libretro/libretro-core-info/refs/heads/master/fbneo_libretro.info"
+
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
+
+if [[ -z "$PS5_PAYLOAD_SDK" ]]; then
+    echo "error: PS5_PAYLOAD_SDK is not set"
+    exit 1
+fi
+
+source "${PS5_PAYLOAD_SDK}/toolchain/prospero.sh" || exit 1
+
+TEMPDIR=$(mktemp -d)
+trap 'rm -rf -- "$TEMPDIR"' EXIT
+
+wget -O $TEMPDIR/FBNeo.tar.gz "${URL}" || exit 1
+tar xf  $TEMPDIR/FBNeo.tar.gz -C $TEMPDIR || exit 1
+
+cd $TEMPDIR/FBNeo-$VER/src/burner/libretro || exit 1
+${MAKE} || exit 1
+
+mkdir -p "${SCRIPT_DIR}/.config/retroarch/cores" || exit 1
+mv $TEMPDIR/FBNeo-$VER//src/burner/libretro/fbneo_libretro.so "${SCRIPT_DIR}/.config/retroarch/cores/" || exit 1
+wget $INFO -O "${SCRIPT_DIR}/.config/retroarch/cores/fbneo_libretro.info"
